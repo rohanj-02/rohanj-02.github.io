e5bb1e39cc91d4acbdf7b526d3a9c8821a511b33
Rohan Jain <rohan19095@iiitd.ac.in>
2020-08-25 21:33:00 +0530

    Github API called
    Have to add promises, to update Data

---
+++ script.js
@@ -0,0 +1,17 @@
+// console.log(Octokit);
+// console.log("Working");
+// const octokit = new Octokit({ auth: `${GITHUB_AUTH_KEY}` });
+
+// async function getCommit() {
+// 	const response = await octokit.request(
+// 		"GET /repos/{owner}/{repo}/commits/{ref}",
+// 		{
+// 			owner: "rohanj-02",
+// 			repo: "chat-app",
+// 			ref: "b95016d9b5cbd901721a9467d210b6cbbeb20af1",
+// 		}
+// 	);
+// 	console.log(response);
+// }
+
+// getCommit();
