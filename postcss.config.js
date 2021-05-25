de96ab6af1a7ce41994a3e95e06eedd5b56fc4b3
Rohan Jain <rohan19095@iiitd.ac.in>
2021-05-25 13:07:27 +0530

    change postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,19 @@ module.exports = {
 	plugins: {
 		tailwindcss: {},
 		autoprefixer: {},
+		...(process.env.NODE_ENV === 'production'
+			? {
+					'@fullhuman/postcss-purgecss': {
+						// added sections folder and changed extension to jsx
+						content: [
+							'./src/**/*.jsx',
+							'./src/**/*.js',
+							'./src/**/*.tsx',
+							'./src/**/*.ts',
+						],
+						defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+					},
+			  }
+			: {}),
 	},
 };
